6e27ee82cab02ba2ac8b30c609fa1b70fcf1ae2a
Kiaraw <kiara.wurtz@epitech.digital>
2026-01-06 11:09:54 +0100

    Init projet

---
+++ front/app/layout.tsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Portfolio",
+  description: "Mon portfolio",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
