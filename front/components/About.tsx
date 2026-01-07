3c9c8f54c469970e12cf65e31af6dcd93753411f
Kiaraw <kiara.wurtz@epitech.digital>
2026-01-07 15:35:02 +0100

    UPD, a propos fini

--- front/components/About.tsx
+++ front/components/About.tsx
@@ -1,7 +1,24 @@
 export default function About() {
     return (
-        <section>
-            <h1>About</h1>
+        <section
+            id="about"
+            className="py-24 bg-white"
+        >
+        <div className="max-w-3xl mx-auto px-6 text-center space-y-6">
+        
+            <h2 className="text-4xl font-bold text-gray-900">
+                Qui suis-je&nbsp;?
+            </h2>
+
+            <p className="text-base leading-relaxed text-gray-700">
+                Au cours de mes études, je développe des projets digitaux en
+                m’intéressant particulièrement à leur structuration, leur cohérence
+                et leur impact utilisateur.
+                <br />
+            Mon objectif est d’évoluer vers des rôles liés à la gestion de projet.
+            </p>
+
+        </div>
         </section>
     );
 }
