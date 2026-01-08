465d18ba0355574a2f258e2a612fa2bcf9d34e31
Kiaraw <kiara.wurtz@epitech.digital>
2026-01-08 15:43:24 +0100

    ADD Footer

--- front/components/Footer.tsx
+++ front/components/Footer.tsx
@@ -1,7 +1,11 @@
 export default function Footer() {
-    return (
-        <section>
-            <h1>Footer</h1>
-        </section>
-    );
+  return (
+    <footer className="bg-gradient-to-br from-[#9ED9C8] to-[#FFF1B8]">
+      <div className="max-w-6xl mx-auto px-6 py-6 text-center">
+        <p className="text-sm text-gray-800">
+          © {new Date().getFullYear()} Kiara Wurtz. Tous droits réservés.
+        </p>
+      </div>
+    </footer>
+  );
 }
